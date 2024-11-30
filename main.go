8e5b91a91d2d2952670156549c7589c47875265a
MisterOryon <misteroryon@protonmail.com>
2024-11-30 15:46:34 +0100

    add: update greeting message in hello function

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello 13\n")
+    fmt.Fprintf(w, "hello 14\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
